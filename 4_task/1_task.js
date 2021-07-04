96c15156519c21db46ac82a36544f7d4104eeaec
Umar Khalilov <ERMASTER100@gmail.com>
2021-07-04 20:56:35 +0300

    Refactor code

--- 4_task/1_task.js
+++ 4_task/1_task.js
@@ -1,68 +1,68 @@
 let testData = [1, 2, 1990, 85, 24, "Vasya", "colya@example.com", "Rafshan", "ashan@example.com",
-  true, false];
+    true, false];
 let testData2 = [1, 2, 1990, 85, 24, 5, 7, 8.1];
 let testData3 = [{
-  "name": "Vasya",
-  "email": "vasya@example.com",
-  "age": 20,
-  "skills": { "php": 0, "js": -1, "madness": 10, "rage": 10 },
+    "name": "Vasya",
+    "email": "vasya@example.com",
+    "age": 20,
+    "skills": {"php": 0, "js": -1, "madness": 10, "rage": 10},
 },
-  { "name": "Dima", "email": "dima@example.com", "age": 34, "skills": { "php": 5, "js": 7, "madness": 3, "rage": 2 } },
-  {
-    "name": "Colya",
-    "email": "colya@example.com",
-    "age": 46,
-    "skills": { "php": 8, "js": -2, "madness": 1, "rage": 4 },
-  },
-  {
-    "name": "Misha",
-    "email": "misha@example.com",
-    "age": 16,
-    "skills": { "php": 6, "js": 6, "madness": 5, "rage": 2 },
-  },
-  {
-    "name": "Ashan",
-    "email": "ashan@example.com",
-    "age": 99,
-    "skills": { "php": 0, "js": 10, "madness": 10, "rage": 1 },
-  },
-  {
-    "name": "Rafshan",
-    "email": "rafshan@example.com",
-    "age": 11,
-    "skills": { "php": 0, "js": 0, "madness": 0, "rage": 10 },
-  }];
-let testData4 = [{ "name": "Vasya", "email": "vasya@example.com", "age": 20 }, {
-  "name": "Dima",
-  "email": "dima@example.com",
-  "age": 34,
+    {"name": "Dima", "email": "dima@example.com", "age": 34, "skills": {"php": 5, "js": 7, "madness": 3, "rage": 2}},
+    {
+        "name": "Colya",
+        "email": "colya@example.com",
+        "age": 46,
+        "skills": {"php": 8, "js": -2, "madness": 1, "rage": 4},
+    },
+    {
+        "name": "Misha",
+        "email": "misha@example.com",
+        "age": 16,
+        "skills": {"php": 6, "js": 6, "madness": 5, "rage": 2},
+    },
+    {
+        "name": "Ashan",
+        "email": "ashan@example.com",
+        "age": 99,
+        "skills": {"php": 0, "js": 10, "madness": 10, "rage": 1},
+    },
+    {
+        "name": "Rafshan",
+        "email": "rafshan@example.com",
+        "age": 11,
+        "skills": {"php": 0, "js": 0, "madness": 0, "rage": 10},
+    }];
+let testData4 = [{"name": "Vasya", "email": "vasya@example.com", "age": 20}, {
+    "name": "Dima",
+    "email": "dima@example.com",
+    "age": 34,
 },
-  { "name": "Colya", "email": "colya@example.com", "age": 46 }, {
-    "name": "Misha",
-    "email": "misha@example.com",
-    "age": 16,
-  },
-  { "name": "Ashan", "email": "ashan@example.com", "age": 99 }, {
-    "name": "Rafshan",
-    "email": "rafshan@example.com",
-    "age": 11,
-  },
-  1, 2, 1990, 85, 24, "Vasya", "colya@example.com", "Rafshan", "ashan@example.com", true, false,
-  [[[[1, 2, 1990, 85, 24, "Vasya", "colya@example.com", "Rafshan", "ashan@example.com", true, false,
-    [{ "name": "Rafshan", "email": "rafshan@example.com", "age": 11 }]]]]]];
+    {"name": "Colya", "email": "colya@example.com", "age": 46}, {
+        "name": "Misha",
+        "email": "misha@example.com",
+        "age": 16,
+    },
+    {"name": "Ashan", "email": "ashan@example.com", "age": 99}, {
+        "name": "Rafshan",
+        "email": "rafshan@example.com",
+        "age": 11,
+    },
+    1, 2, 1990, 85, 24, "Vasya", "colya@example.com", "Rafshan", "ashan@example.com", true, false,
+    [[[[1, 2, 1990, 85, 24, "Vasya", "colya@example.com", "Rafshan", "ashan@example.com", true, false,
+        [{"name": "Rafshan", "email": "rafshan@example.com", "age": 11}]]]]]];
 
 // 1. Напишите функцию cloneDeep таким образом, чтобы она была способна клонировать переданный как параметр объект.
 
-const cloneDeep=(obj)=> {
-  const clObj = {};
-  for(const key in obj) {
-    if (obj[key] instanceof Object) {
-      clObj[key] = cloneDeep(obj[key]);
-      continue;
+const cloneDeep = (obj) => {
+    const clObj = {};
+    for (const key in obj) {
+        if (obj[key] instanceof Object) {
+            clObj[key] = cloneDeep(obj[key]);
+            continue;
+        }
+        clObj[key] = obj[key];
     }
-    clObj[key] = obj[key];
-  }
-  return clObj;
+    return clObj;
 }
 
 console.log(cloneDeep(testData4));
